4a784ef80ee986f3ddb2788deef37c5b8c202238
Rashed Abdel-Tawab <rashed@linux.com>
2019-09-05 21:35:38 -0700

    core: Only include Lineage SE policy if repo exists
    Change-Id: If799106d2da6d8c303430e316edff365a22f7c99

--- core/config.mk
+++ core/config.mk
@@ -1185,9 +1185,11 @@ DEFAULT_DATA_OUT_MODULES := ltp $(ltp_packages) $(kselftest_modules)
 .KATI_READONLY := DEFAULT_DATA_OUT_MODULES
 
 ifneq ($(KCUF_BUILD),)
+ifneq ($(wildcard device/kcuf/sepolicy/common/sepolicy.mk),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
 $(eval include device/kcuf/sepolicy/common/sepolicy.mk)
+endif
 endif
 
 # Include any vendor specific config.mk file
